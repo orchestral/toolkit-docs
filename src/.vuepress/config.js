401febeae503f8890e3ed7069b194e1c73b23141
Mior Muhammad Zaki <crynobone@gmail.com>
2021-01-27 23:13:51 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Testbench Toolkit',
+  title: 'Laravel Packages Toolkit',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
